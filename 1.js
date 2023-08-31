b35d8258844fa294d9b98086790c835c2d4d13c7
S4vi0r <gbp17@outlook.com>
2023-08-30 21:24:14 -0500

    xd

---
+++ 1.js
@@ -0,0 +1,8 @@
+let listaCompras = [["Cereal",3],["Leche",2],["Huevos",12],["Pan",1],["Queso",1],["Jamon",1]];
+
+console.log("Voy a comprar " + listaCompras[0][1] + " paquetes de " + listaCompras[0][0]);
+console.log("Voy a comprar " + listaCompras[1][1] + " litros de " + listaCompras[1][0]);   
+
+// ...
+
+// Siguente tema: Funciones
