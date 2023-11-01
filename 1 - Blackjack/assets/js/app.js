7a016d11286ec8c6ec2e4126ad624151d85bd579
S4vi0r <gbp17@outlook.com>
2023-11-01 12:20:21 -0500

    Lógica finalizada 🥴

--- 1 - Blackjack/assets/js/app.js
+++ 1 - Blackjack/assets/js/app.js
@@ -15,10 +15,13 @@ let puntosJugador = 0, puntosComputadora = 0;
 // Referecias HTML
 
 const btn_pedir = document.querySelector('#btn-pedir');
+const btn_detener = document.querySelector('#btn-detener');
+const btn_nuevo = document.querySelector('#btn-nuevo');
 
 const smalls = document.querySelectorAll('small')
 
 const divCartarPlayer = document.querySelector('#player-card')
+const divCartarComputer = document.querySelector('#computer-card')
 
 
 
@@ -61,6 +64,46 @@ const valorCarta = (carta) => {
     return (isNaN(valor)) ? (( valor === 'A') ? 11 : 10) : parseInt(valor);
 }
 
+// Turno de la computadora
+const turnoComputadora = (puntosMinimos) => {
+    
+    do {
+            
+        const carta = pedirCarta();
+        puntosComputadora += valorCarta(carta);
+
+        smalls[1].innerText = puntosComputadora;
+
+
+        // crear img
+        const imgCard = document.createElement('img');
+
+        imgCard.src = `assets/cartas/${carta}.png`;
+        imgCard.classList.add('card');
+
+        divCartarComputer.append(imgCard);
+
+        if (puntosMinimos > 21) {
+            break;
+        }
+
+    } while (puntosComputadora < puntosMinimos && puntosMinimos <=21);
+
+    setTimeout(() => {
+        if (puntosComputadora === puntosMinimos){
+            alert('Nadia gana');
+        } else if (puntosMinimos > 21){
+            alert('Computadora gana');
+        } else if (puntosComputadora > 21){
+            alert('jugador gana');
+        } else{
+            alert('Computadora gana');
+        }
+    }, 10);
+
+
+}
+
 
 const valor = valorCarta(pedirCarta());
 console.log(deck);
@@ -88,8 +131,35 @@ btn_pedir.addEventListener('click', () => {
     if(puntosJugador > 21){
         console.warn('Perdiste :\'v');
         btn_pedir.disabled = true;
+        btn_detener.disabled = true;
+        turnoComputadora(puntosJugador);
+        
     }else if(puntosJugador === 21){
         console.warn('21, cool');
-        btn_pedir.disabled = true;
+        btn_pedir.disabled = true;    btn_detener.disabled = true;
+        btn_detener.disabled = true;
+        turnoComputadora(puntosJugador);
     }
 });
+
+btn_detener.addEventListener('click', () => {
+    btn_detener.disabled = true;
+    btn_pedir.disabled = true;
+    turnoComputadora(puntosJugador);
+})
+
+btn_nuevo.addEventListener('click', () => {
+    deck = crearDeck();
+
+    puntosComputadora = 0;
+    puntosJugador = 0;
+    
+    smalls[0].innerText = 0;
+    smalls[1].innerText = 0;
+
+    divCartarComputer.innerHTML = '';
+    divCartarPlayer.innerHTML = '';
+
+    btn_detener.disabled = false;
+    btn_pedir.disabled = false;
+})
