1cff34ee73294084a381032dd346875f5ec8fb5a
S4vi0r <gbp17@outlook.com>
2024-01-08 20:58:04 -0500

    Design Patterns

---
+++ test.py
@@ -0,0 +1,29 @@
+[
+    [
+        "AMERICA",
+        [
+            ["PERÚ", ["Lima", "Arequipa", "Chimbote", "Trujillo", "Cajamarca"]],
+            ["ARGENTINA", ["Cordova", "Rosario", "Mendoza", "Tucuman"]],
+            ["MÉXICO", ["Guadalajara", "Monterrey", "Hidalgo", "Yucatan", "Jalisco"]],
+            ["BRASIL", ["Bahia"]],
+            ["COLOMBIA", ["Bogota", "Cali", "Barranquilla", "Medellín"]],
+        ],
+    ],
+    [
+        "EUROPA",
+        [
+            ["ESPAÑA", ["Madrid", "Barcelona", "Valencia", "Sevilla", "Granada"]],
+            ["FRANCIA", ["Paris", "Marsella", "Lyon", "Niza", "Burdeos"]],
+            ["ITALIA", ["Roma", "Florencia", "Milan", "Verona", "Turin"]],
+            ["ALEMANIA", ["Berlin", "Munich", "Hamburgo", "Bonn"]],
+        ],
+    ],
+    [
+        "ASIA",
+        [
+            ["CHINA", ["Pekin", "Shangai", "Canton"]],
+            ["JAPÓN", ["Tokio", "Osaka", "Yokohama"]],
+            ["INDIA", ["Calcuta", "Bombay"]],
+        ],
+    ],
+]
