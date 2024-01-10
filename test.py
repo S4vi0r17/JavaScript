61ad90aada1901c25e47b5a9371b363106c33890
S4vi0r <gbp17@outlook.com>
2024-01-10 17:13:14 -0500

    Update country and city names in test.py

--- test.py
+++ test.py
@@ -1,32 +1,32 @@
-# [
-#     [
-#         "AMERICA",
-#         [
-#             ["PERÚ", ["Lima", "Arequipa", "Chimbote", "Trujillo", "Cajamarca"]],
-#             ["ARGENTINA", ["Cordova", "Rosario", "Mendoza", "Tucuman"]],
-#             ["MÉXICO", ["Guadalajara", "Monterrey", "Hidalgo", "Yucatan", "Jalisco"]],
-#             ["BRASIL", ["Bahia"]],
-#             ["COLOMBIA", ["Bogota", "Cali", "Barranquilla", "Medellín"]],
-#         ],
-#     ],
-#     [
-#         "EUROPA",
-#         [
-#             ["ESPAÑA", ["Madrid", "Barcelona", "Valencia", "Sevilla", "Granada"]],
-#             ["FRANCIA", ["Paris", "Marsella", "Lyon", "Niza", "Burdeos"]],
-#             ["ITALIA", ["Roma", "Florencia", "Milan", "Verona", "Turin"]],
-#             ["ALEMANIA", ["Berlin", "Munich", "Hamburgo", "Bonn"]],
-#         ],
-#     ],
-#     [
-#         "ASIA",
-#         [
-#             ["CHINA", ["Pekin", "Shangai", "Canton"]],
-#             ["JAPÓN", ["Tokio", "Osaka", "Yokohama"]],
-#             ["INDIA", ["Calcuta", "Bombay"]],
-#         ],
-#     ],
-# ]
+[
+    [
+        "AMERICA",
+        [
+            ["PERÚ", ["Lima", "Arequipa", "Chimbote", "Trujillo", "Cajamarca"]],
+            ["ARGENTINA", ["Cordova", "Rosario", "Mendoza", "Tucuman"]],
+            ["MÉXICO", ["Guadalajara", "Monterrey", "Hidalgo", "Yucatan", "Jalisco"]],
+            ['BRASIL' [ 'Sao Paulo', 'Rio de Janeiro' ,'Porto Alegre' , 'Bahia' , 'Belo Horizonte']],
+            ["COLOMBIA", ["Bogota", "Cali", "Barranquilla", "Medellín"]],
+        ],
+    ],
+    [
+        "EUROPA",
+        [
+            ["ESPAÑA", ["Madrid", "Barcelona", "Valencia", "Sevilla", "Granada"]],
+            ["FRANCIA", ["Paris", "Marsella", "Lyon", "Niza", "Burdeos"]],
+            ["ITALIA", ["Roma", "Florencia", "Milan", "Verona", "Turin"]],
+            ["ALEMANIA", ["Berlin", "Munich", "Hamburgo", "Bonn"]],
+        ],
+    ],
+    [
+        "ASIA",
+        [
+            ["CHINA", ["Pekin", "Shangai", "Canton"]],
+            ["JAPÓN", ["Tokio", "Osaka", "Yokohama"]],
+            ["INDIA", ["Calcuta", "Bombay"]],
+        ],
+    ],
+]
 
 
 
