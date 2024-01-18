7ff032756124365755cfa5993888a0997591a8f6
S4vi0r <gbp17@outlook.com>
2024-01-17 21:19:40 -0500

    Project initialized and colors configured

---
+++ 54-loan-quotation-react/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      pinkLavender: "#f6e3ec",
+      lightPink: "#ffd9de",
+      mauve: "#a46b88"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
