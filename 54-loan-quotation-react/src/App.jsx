159bd6cfa3dfd402a6b81d6dd35665cc573445ac
S4vi0r <gbp17@outlook.com>
2024-01-17 22:18:57 -0500

    Add input range element to App component

--- 54-loan-quotation-react/src/App.jsx
+++ 54-loan-quotation-react/src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
+      <input
+        type="range"
+        className="w-full h-6 accent-mauve hover:accent-mauve"
+      />
     </div>
   )
 }
