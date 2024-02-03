e4718383c97643f9ba708494b125b4ff4ecf3a89
S4vi0r <gbp17@outlook.com>
2024-02-03 11:11:26 -0500

    Refactor HTML and CSS in index.html and Header.jsx

--- 56-FullStackJS/frontend/src/components/Header.jsx
+++ 56-FullStackJS/frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     const { logout } = useAuth();
 
 	return (
-		<header className='py-10 bg-brown-500 w-screen'>
+		<header className='py-10 bg-brown-500'>
 			<div className='container mx-auto flex flex-col lg:flex-row justify-between items-center'>
 				<h1 className='font-bold text-3xl text-brown-200 text-center'>
 					Patient Manager for {''}
