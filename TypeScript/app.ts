45b7fb03903fffbdc93c71a6be4e1280e0c3a6f7
S4vi0r <gbp17@outlook.com>
2024-02-17 11:21:39 -0500

    Interfaces in TypeScript

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -2,61 +2,62 @@
 
 // Cree una interfaz para validar el auto (el valor enviado por parametro)
 interface Auto {
-	encender: boolean;
-	velocidadMaxima: number;
-	acelear(): void;
+	encender: boolean
+	velocidadMaxima: number
+	acelear(): void
 }
 
 const conducirBatimovil = (auto: Auto): void => {
-	auto.encender = true;
-	auto.velocidadMaxima = 100;
-	auto.acelear();
-};
+	auto.encender = true
+	auto.velocidadMaxima = 100
+	auto.acelear()
+}
 
 const batimovil: Auto = {
 	encender: false,
 	velocidadMaxima: 0,
 	acelear() {
-		console.log('...... gogogo!!!');
+		console.log('... ... gogogo!!!')
 	},
-};
+}
 
 // Cree una interfaz con que permita utilizar el siguiente objeto
 // utilizando propiedades opcionales
 interface Guason {
-	reir?: boolean;
-	comer?: boolean;
-	llorar?: boolean;
+	reir?: boolean
+	comer?: boolean
+	llorar?: boolean
 }
 
 const guason: Guason = {
 	reir: true,
 	comer: true,
 	llorar: false,
-};
+}
 
 const reir = (guason: Guason): void => {
 	if (guason.reir) {
-		console.log('JAJAJAJA');
+		console.log('JAJAJAJA')
 	}
-};
+}
 
 // Cree una interfaz para la siguiente funcion
 interface CiudadGoticaFn {
-	(ciudadanos: string[]): number;
+	(ciudadanos: string[]): number
 }
+
 const ciudadGotica: CiudadGoticaFn = (ciudadanos: string[]): number => {
-	return ciudadanos.length;
-};
+	return ciudadanos.length
+}
 
 // Cree una interfaz que obligue crear una clase
 // con las siguientes propiedades y metodos
 interface PersonaInterface {
-	edad: number;
-	estadoCivil: string;
-	nombre: string;
-	sexo: string;
-	imprimirBio(): void;
+	edad: number
+	estadoCivil: string
+	nombre: string
+	sexo: string
+	imprimirBio(): void
 }
 /*
   propiedades:
@@ -67,10 +68,10 @@ interface PersonaInterface {
     - imprimirBio(): void // en consola una breve descripcion.
 */
 class Persona implements PersonaInterface {
-	public edad: number;
-	public estadoCivil: string;
-	public nombre: string;
-	public sexo: string;
+	public edad: number
+	public estadoCivil: string
+	public nombre: string
+	public sexo: string
 
 	imprimirBio() {}
 }
